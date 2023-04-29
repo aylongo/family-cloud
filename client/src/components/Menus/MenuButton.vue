99186e1c63ba74ce523a564ab0c06fcfb4bace33
aylongo <aylongo10@gmail.com>
2023-04-29 15:49:05 +0300

    feat(): added MenuButton component

--- client/src/components/Menus/MenuButton.vue
+++ client/src/components/Menus/MenuButton.vue
@@ -1,11 +1,22 @@
 <template>
-    
+  <v-menu
+    v-model="menu"
+    :close-on-content-click="true"
+    :nudge-width="150"
+    offset-x
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn icon v-bind="attrs" v-on="on">
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </template>
+
+    <v-list> </v-list>
+  </v-menu>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style scoped>
