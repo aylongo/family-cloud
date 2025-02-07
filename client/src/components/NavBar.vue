7d8aad9bdd81835b6f285b966a257526a78c4983
aylongo <aylongo10@gmail.com>
2025-02-07 03:11:37 +0200

    fix(): removed side bar

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,43 +1,9 @@
 <template>
   <div @click.right.prevent>
-    <v-app-bar class="nav-bar-text" color="indigo" elevation="2" dark>
-      <v-icon class="pa-3" large>mdi-cloud-tags</v-icon>
+    <v-app-bar class="nav-bar-text" color="indigo lighten-3" elevation="0" dark>
+      <v-icon class="pr-4 pl-3" large>mdi-cloud-tags</v-icon>
       <v-toolbar-title>ענן משפחתי</v-toolbar-title>
-
-      <v-spacer />
-
-      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
     </v-app-bar>
-
-    <v-navigation-drawer v-model="drawer" absolute temporary>
-      <v-container>
-        <v-row>
-          <v-col class="text-center">
-            <span class="text-h5">תפריט</span>
-          </v-col>
-        </v-row>
-      </v-container>
-
-      <v-list nav dense>
-        <v-list-item-group
-          v-model="group"
-          active-class="indigo--text text--accent-4"
-        >
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>בית</v-list-item-title>
-          </v-list-item>
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-folder-arrow-up</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>העלאה</v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
   </div>
 </template>
 
