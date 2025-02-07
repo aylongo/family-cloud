0d9bee2c6c38c885996547b946afc6b91afa2608
aylongo <aylongo10@gmail.com>
2025-02-07 03:15:36 +0200

    fix(): removed shit

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,46 +1,18 @@
 <template>
   <v-container @click.right.prevent>
-    <v-row justify="center" align="center">
-      <v-col>
-        <file-search />
-      </v-col>
-
-      <v-col>
-        <file-upload />
-      </v-col>
-    </v-row>
-
     <v-row justify="center">
       <v-col>
         <file-view />
       </v-col>
     </v-row>
-
-    <div class="text-center pa-8">
-      <h2>משימות שנשארו</h2>
-      <p>
-        עדכון המסך לאחר העלאה/מחיקה של קבצים
-        <br />
-        ליצור קומפוננטה של כפתור (שלוש נקודות) לפתיחת תפריט + קומפוננטה עוטפת לכפתור תפריט עבור קובץ ועבור תיקייה בנפרד
-        <br />
-        יצירת תיקייה באמצעות מניו עם קליק ימני
-        <br />
-        למחוק קבצים (הוספת כפתור שלוש נקודות ואופציה ללחיצה עם מקש ימני על קובץ)
-        <br />
-        לעבור בין תיקיות ולהיכנס לתיקיות
-        <br />
-        לחפש קבצים
-        <br />
-        עדכון קובץ אם הועלה קובץ קיים (לדרוס את המידע הקיים בקובץ במידע החדש.
-        כרגע המידע נוסף לסוף הקובץ)
-        <br />
-        שיפור עיצוב
-        <br />
-        להעלות קבצים בעברית
-        <br />
-        לארגן את הרפוסיטורי
-      </p>
-    </div>
+    <v-row justify="center" align="center">
+      <v-col>
+        <file-search />
+      </v-col>
+      <v-col>
+        <file-upload />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
